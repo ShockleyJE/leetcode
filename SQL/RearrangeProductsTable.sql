5e34380161888aa9ead40c774539bd6f621e2c17
shockleyje <8533782+ShockleyJE@users.noreply.github.com>
2022-11-02 07:23:20 -0400

    Rearrange Product Table (SQL)

---
+++ SQL/RearrangeProductsTable.sql
@@ -0,0 +1,20 @@
+SELECT 
+product_id
+,'store1' as store
+,store1 as price
+FROM Products p
+WHERE store1 is not null
+UNION
+SELECT 
+product_id
+,'store2' as store
+,store2 as price
+FROM Products p
+WHERE store2 is not null
+UNION
+SELECT 
+product_id
+,'store3' as store
+,store3 as price
+FROM Products p
+WHERE store3 is not null
