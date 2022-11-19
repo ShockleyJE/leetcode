dd6ad147e38903fbf661bbab82fc609a7dd1bb81
shockleyje <8533782+ShockleyJE@users.noreply.github.com>
2022-11-19 12:55:28 -0500

    Customers Who Bought Stuff (Easy, SQL)

--- SQL/Subqueries/CustomersWhoNeverOrdered.sql
+++ SQL/Subqueries/CustomersWhoNeverOrdered.sql
@@ -1,3 +1,9 @@
 SELECT c.name AS Customers 
 FROM Customers c
 WHERE c.id NOT IN (select DISTINCT(customerId) from Orders);
+
+-- Write your MySQL query statement below
+SELECT v.customer_id, count(visit_id) AS count_no_trans
+FROM Visits v
+WHERE v.visit_id NOT IN (SELECT visit_id FROM Transactions)
+GROUP BY v.customer_id
